e7e70f0677762f1b1299b1e4001cdc066d0a0dc4
sdevasekhar <devasekharpmt@gmail.com>
2019-02-20 11:54:19 +0530

    initial singleton

--- designpatterns/cpp/singleton.cpp
+++ designpatterns/cpp/singleton.cpp
@@ -1,5 +1,30 @@
 #include<iostream>
-int main()
+
+class Singleton
 {
-return 0;
-}
+private:
+	Singleton() = default;
+	~Singleton()
+	{
+		delete obj;
+	}
+public:
+	Singleton(const Singleton&) = delete;
+	Singleton(Singleton &&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+	Singleton& operator=(Singleton &&) = delete;
+public:
+	static Singleton* getInstance()
+	{
+		if (obj == nullptr)
+		{
+			obj = new Singleton();
+			std::cout << "object cretaed" << std::endl;
+		}
+		std::cout << "object returned" << std::endl;
+		return obj;
+	}
+
+private:
+	static Singleton * obj;
+};
